c49eaaea8128e37ca472f5a9ddabc80666169f26
ChadKreutzer <kanthalion@gmail.com>
2016-11-29 12:54:11 -0700

    add win state test

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,71 @@
-# Main Game loop
+# Main Game
 class Game
+  def initialize
+    @winner = false
+    @tie = false
+    @game_board = Board.new
+    @player_1 = Player.new.piece = 'X'
+    @player_2 = Player.new.piece = 'O'
+  end
+
+  def win_or_tie(board)
+    win_state = :none
+    [diagonal?(board), horizontal?(board), vertical?(board)].each do |x|
+      win_state = :win if x
+    end
+    win_state = :tie if tie_game?(board)
+    win_state
+  end
+
+  private
+
+  def diagonal?(board)
+    if [board[0], board[4], board[8]].uniq.count == 1
+      true
+    elsif [board[2], board[4], board[6]].uniq.count == 1
+      true
+    else
+      false
+    end
+  end
+
+  def horizontal?(board)
+    if [board[0], board[1], board[2]].uniq.count == 1
+      true
+    elsif [board[3], board[4], board[5]].uniq.count == 1
+      true
+    elsif [board[6], board[7], board[8]].uniq.count == 1
+      true
+    else
+      false
+    end
+  end
+
+  def vertical?(board)
+    if [board[0], board[3], board[6]].uniq.count == 1
+      true
+    elsif [board[1], board[4], board[7]].uniq.count == 1
+      true
+    elsif [board[2], board[5], board[8]].uniq.count == 1
+      true
+    else
+      false
+    end
+  end
+
+  def tie_game?(board)
+    tie = true
+    (0..8).each { |x| tie = false if board[x].is_a?(Numeric) }
+    tie
+  end
 end
 
 # The Game Board
 class Board
-  attr_accessor :board, :winner
+  attr_accessor :board
 
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @winner = false
   end
 
   def display_board
@@ -19,15 +76,13 @@ class Board
     " #{@board[6]} | #{@board[7]} | #{@board[8]}\n" \
   end
 
-  def player_move(spot, piece)
+  def place_piece(spot, piece)
     @board[spot] = piece
     @board
-  end
-
-  def win_test(current_board)
   end
 end
 
 # Each player
 class Player
+  attr_accessor :piece
 end
--- test/tic_tac_toe_test.rb
+++ test/tic_tac_toe_test.rb
@@ -18,13 +18,18 @@ class TicTacTest < Minitest::Test
     assert_equal expected, ::Board.new.display_board
   end
 
-  def test_player_move
+  def test_adding_piece_to_board
     expected = [1, 2, 3, 4, 'X', 6, 7, 8, 9]
-    assert_equal expected, ::Board.new.player_move(4, 'X')
+    assert_equal expected, ::Board.new.place_piece(4, 'X')
   end
 
-  def test_win_state
-    expected = [true, 'X']
-    assert_equal expected, ::Board.new.win_test(['X', 2, 3, 4, 'X', 6, 7, 8, 'X'])
+  def test_winner
+    expected = :win
+    assert_equal expected, ::Game.new.win_or_tie(['X', 2, 3, 4, 'X', 6, 7, 8, 'X'])
+  end
+
+  def test_player_piece_assignment
+    expected = 'X'
+    assert_equal expected, ::Player.new.piece = 'X'
   end
 end
