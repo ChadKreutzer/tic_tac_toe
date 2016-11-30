b6031f2d0b2928ebf49aabeb36107d342e6e5f5e
Chad Kreutzer <kanthalion@gmail.com>
2016-11-30 13:22:21 +0000

    Fix argument number error for WinTest tests

--- lib/win_test.rb
+++ lib/win_test.rb
@@ -1,5 +1,12 @@
 # Game logic
 class WinTest
+  attr_reader :board
+  
+  def initialize(board)
+    @board = board
+  end
+  
+  
   WINNERS = [[0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5],
              [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]].freeze
   
